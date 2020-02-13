9e4cad81341f9bed6f0f29c074748d25e43a3ce7
QA3R <46014750+QA3R@users.noreply.github.com>
2020-02-13 17:43:43 -0500

    Added base combat functionality

---
+++ VRProject_EmergTecs/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int parryCount;
+    public int hitWindow;
+    public int health;
+
+    public bool isImmortal;
+
+    public TextMeshProUGUI healthCounter;
+    public TextMeshProUGUI parryCounter;
+    public TextMeshProUGUI enemyState;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        isImmortal = true;
+        parryCount = 7;
+        hitWindow = 7;
+        health = 10;
+        parryCounter.text = "PARRY COUNT: " + parryCount.ToString();
+        healthCounter.text = "HEALTH: " + health.ToString();
+        enemyState.text = "STATE: DEFENDING";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecreaseParryCounter()
+    {
+        parryCount --;
+        ChangeState();
+        parryCounter.text = "PARRY COUNT: " + parryCount.ToString();
+        Debug.Log("The enemy can block " + parryCount + " more times.");
+    }
+
+    public void ChangeState()
+    {
+        if (isImmortal && parryCount <= 0)
+        {
+            isImmortal = false;
+            hitWindow = 7;
+            Debug.Log("THE ENEMY IS EXPOSED");
+            enemyState.text = "STATE: EXPOSED";
+        }
+        else if (!isImmortal && hitWindow <=0) 
+        {
+            isImmortal = true;
+            parryCount = 7;
+            Debug.Log("THE ENEMY ENTERED ITS DEFENSE STATE");
+            enemyState.text = "STATE: DEFENDING";
+        }
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Weapon" && !isImmortal)
+        {
+            hitWindow--;
+            health--;
+            ChangeState();
+            Debug.Log("The enemy has " + health + " health left.");
+            healthCounter.text = "HEALTH: " + health.ToString();
+        }
+        else if (collision.gameObject.tag == "Weapon" && isImmortal)
+        {
+            Debug.Log("THE ENEMY IS IN DEFENSE STATE");
+        }
+    }
+}
---
+++ VRProject_EmergTecs/Assets/Scripts/SwordScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    public GameObject enemyScriptObj;
+    public EnemyScript enemyScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyScriptObj = GameObject.FindGameObjectWithTag("Enemy");
+        enemyScript = enemyScriptObj.GetComponent<EnemyScript>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy_Sword" && enemyScript.isImmortal)
+        {
+            Debug.Log("Sword has collided");
+            enemyScript.DecreaseParryCounter();
+        }
+        else
+        {
+            Debug.Log("Attack was blocked");
+        }
+    }
+}
